7058690e067e804b9b729e99a596fa2786379182
Grzegorz Gumieniak <kontaktg.gumieniak@gmail.com>
2021-12-05 14:48:04 +0100

    Initial Commit

---
+++ TeamProject/TeamProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamProjectApp.swift
+//  TeamProject
+//
+//  Created by Grzegorz Gumieniak on 05/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TeamProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
