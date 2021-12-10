6c4dbf71a2abd83717a93dde28706ca7846f2c7d
Grzegorz Gumieniak <kontaktg.gumieniak@gmail.com>
2021-12-10 14:36:55 +0100

    Utworzenie modelu przechwytywania danych

--- TeamProject/Model/BudgetFetcher.swift
+++ TeamProject/Model/BudgetFetcher.swift
@@ -17,3 +17,46 @@ protocol BudgetFetcher {
 	func getUserExpensives() async throws -> [PLN]
 	func getPredictionforType(_ id: ID, budget: Budget) async throws -> Double
 }
+
+class BudgetFetcherImpl: BudgetFetcher {
+	private let session: URLSession
+	
+	init() {
+		self.session = URLSession(configuration: .ephemeral)
+	}
+	
+	func getArrayOfBudgetType() async throws -> [BudgetType] {
+		return BudgetType.arrayOfBudgetTypes
+	}
+	
+	func getArrayOfBudgetsOf(type: ID) async throws -> [Budget] {
+		return Budget.listOfBudgetMock
+	}
+	
+	func getBudgetBy(_ id: ID) async throws -> Budget {
+		return Budget.budgetMock
+	}
+	
+	func getUserBudget() async throws -> PLN {
+		return 10.0
+	}
+	
+	func getUserExpensives() async throws -> [PLN] {
+		[-10.0, -5.1, -3.2]
+	}
+	
+	func getPredictionforType(_ id: ID, budget: Budget) async throws -> Double {
+		return 0.8
+	}
+	
+	
+}
+
+extension BudgetFetcherImpl {
+	enum REST: String {
+		case GET = "GET"
+		case PUT = "PUT"
+		case DELETE = "DELETE"
+		case POST = "POST"
+	}
+}
